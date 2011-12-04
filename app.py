b761f32090ed6759cdff1206fe6bbc76538ed707
Jim Gumbley <j.gumbley@gmail.com>
2011-12-04 21:52:38 +0000

    integrate bootstrap css framework

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Picture Postcards\n'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
