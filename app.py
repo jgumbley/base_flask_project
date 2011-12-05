d1345fb2c9d00f356e6884dbb43d97f389ddce62
Jim Gumbley <j.gumbley@gmail.com>
2011-12-05 22:51:50 +0000

    some error handling around database connectivity

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask import render_template
+from sqlalchemy.exc import ProgrammingError, OperationalError
 from orm import orm, DatabaseVersion
 
 app = Flask(__name__)
 
 # sqlalchemy config:
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://greetings_dev:netto@localhost:5432/greetings_dev'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://greetings_dev:ne!tto@localhost:5432/greetings_dev'
 orm.init_app(app)
 
 @app.route('/')
@@ -14,8 +15,14 @@ def index():
 
 @app.route('/sysadmin')
 def sysadmin():
-    database_version = orm.session.query(DatabaseVersion).first()
-    return render_template('sysadmin.html', db_ver_num = database_version.version)
+    try:
+        database_version = orm.session.query(DatabaseVersion).first().version
+    except OperationalError:
+        database_version = "cannot connect to db"
+    except ProgrammingError:
+        database_version = "not configured"
+    return render_template('sysadmin.html', db_ver_num = database_version)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
