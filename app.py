63f06d07c0ab325e9f0b5b89b5d1304fb2d63ae9
Jim Gumbley <j.gumbley@gmail.com>
2011-12-06 22:08:48 +0000

    database connection working!

--- app.py
+++ app.py
@@ -6,12 +6,21 @@ from orm import orm, DatabaseVersion
 
 app = Flask(__name__)
 
+# put this in a root config module, the dependencies on the database are unpredictable so needs to be elemental
 try:
+    """ in order to support ep.io postgres settings
+    """
     from bundle_config import config
     conn_url = 'postgresql://' + config['postgres']["username"] + ":" + config['postgres']["password"] + "@" \
               + config['postgres']["host"] + ":" + config['postgres']["port"] + "/" + config['postgres']["database"]
     test = "got it"
+except KeyError:
+    """ this is the case where the ep.io settings are present but not being referenced correctly
+    """
+    pass
 except ImportError:
+    """ this is the case we are not running on ep.io!
+    """
     conn_url = 'postgresql://greetings_dev:netto@localhost:5432/greetings_dev'
     test = "not got it"
 
