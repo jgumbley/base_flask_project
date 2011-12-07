c430d7143195f3375c55886a4c43f6a2af5e7150
Jim Gumbley <jgumbley@medquist.com>
2011-12-07 11:48:19 +0000

    experiment with logging to loggly

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import render_template
-from sqlalchemy.exc import ProgrammingError, OperationalError
+from logbook.handlers import SyslogHandler
 from werkzeug.utils import redirect
 from orm import orm
 from database.api import DatabaseSchema
+
+from logbook import warning
 
 app = Flask(__name__)
 
@@ -12,8 +14,16 @@ from config import conn_url
 app.config['SQLALCHEMY_DATABASE_URI'] = conn_url
 orm.init_app(app)
 
+
+try:
+    syslog_handler = SyslogHandler(application_name="GreetingsFrom", address="logs.loggly.com:28712" \
+                                  ,level='WARNING')
+except :
+    pass
+
 @app.route('/')
 def index():
+    warning("this is a warning")
     # version = orm.session.query(DatabaseVersion).first().version
     return render_template('index.html' )
 
