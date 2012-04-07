e48c69fca23a90aa83eb04b1496f07e0272018dd
unknown <Steve@Steve-PC.(none)>
2012-04-07 19:09:25 +0100

    additional upload file fixups

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ try:
     from bundle_config import config
     conn_url = 'postgresql://' + config['postgres']["username"] + ":" + config['postgres']["password"] + "@" \
               + config['postgres']["host"] + ":" + config['postgres']["port"] + "/" + config['postgres']["database"]
-    test = "got it"
     upload_path = config['core']['data_directory']
 except KeyError:
     """ this is the case where the ep.io settings are present but not being referenced correctly
@@ -15,7 +14,9 @@ except KeyError:
 except ImportError:
     """ this is the case we are not running on ep.io!
     """
+    import os
     conn_url = 'postgresql://dev_role:dev_role@localhost:5432/flask_bitchez'
-    upload_path = "C:\\Users\\Steve\\Desktop\\projects\\postcode-website\\static\\devuploads\\"
-    test = "not got it"
+    # this is not 100% reliable but its just for Dev fallback
+    proj_root = os.path.dirname(__file__)
+    upload_path = os.path.join(proj_root, "static\uploads")
 
