97605bd3c570546f878c918671a7f0c9b66b97c8
Leonig Mig <j.gumbley@gmail.com>
2012-04-21 16:19:39 +0100

    alternate database url

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # all config related stuff in this root module
 import os
 
-conn_url = os.environ.get('SHARED_DATABASE_URL', 'postgresql://dev_role:dev_role@localhost:5432/flask_bitchez')
+conn_url = os.environ.get('DATABASE_URL', 'postgresql://dev_role:dev_role@localhost:5432/flask_bitchez')
 print conn_url
 upload_path = "somewhere"
 
