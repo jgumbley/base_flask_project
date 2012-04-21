52026950594aa21a3d088cc4870ab0dd3967f010
Leonig Mig <j.gumbley@gmail.com>
2012-04-21 16:14:34 +0100

    need to run locally before delivering

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 # all config related stuff in this root module
+import os
 
 conn_url = os.environ.get('SHARED_DATABASE_URL', 'postgresql://dev_role:dev_role@localhost:5432/flask_bitchez')
 upload_path = "somewhere"
