ab4ec7ae4f6307ea1a977144dda96a6b74f5d73e
Leonig Mig <j.gumbley@gmail.com>
2012-04-21 16:06:08 +0100

    switch to dev server + debug

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn app:app -b 0.0.0.0:$PORT -w 3
+web: python app.py
+#web: gunicorn app:app -b 0.0.0.0:$PORT -w 3
--- app.py
+++ app.py
@@ -86,4 +86,6 @@ def list_postcards():
                            user=session.get("user"))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
