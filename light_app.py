b9591da15bc81c36dc5f3bb5ce2a6344673eb47e
Leonig Mig <j.gumbley@gmail.com>
2012-04-21 14:25:55 +0100

    updates just using the flask dev server

--- light_app.py
+++ light_app.py
@@ -1,3 +1,4 @@
+import os
 from flask.app import Flask
 from flask.templating import render_template
 
@@ -11,5 +12,6 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
 
