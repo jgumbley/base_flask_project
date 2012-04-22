45315fa3324b37913e1301d39378513ccba6a15a
Leonig Mig <j.gumbley@gmail.com>
2012-04-22 16:07:25 +0100

    further tweaking config

--- admin.py
+++ admin.py
@@ -2,9 +2,12 @@ from flask import Blueprint
 from flask.templating import render_template
 from werkzeug.utils import redirect
 from authentication import requires_admin
-from config import conn_url
 from database.api import DatabaseSchema
 from orm import User
+from flask.globals import current_app
+
+def conn_url():
+    return current_app.config["SQLALCHEMY_DATABASE_URI"]
 
 sysadmin_pages = Blueprint('sysadmin', __name__)
 
@@ -26,19 +29,19 @@ def make_mod(user):
 @sysadmin_pages.route('/sysadmin/')
 @requires_admin
 def status():
-    schema = DatabaseSchema(conn_url)
+    schema = DatabaseSchema(conn_url())
     return render_template('sysadmin.html', db_ver_num = schema.status() )
 
 @sysadmin_pages.route('/sysaction/initiate-schema')
 @requires_admin
 def sysaction_initiate_schema():
-    schema = DatabaseSchema(conn_url)
+    schema = DatabaseSchema(conn_url())
     schema.initiate()
     return redirect("/sysadmin/")
 
 @sysadmin_pages.route('/sysaction/update-schema')
 @requires_admin
 def sysaction_updateschema():
-    schema = DatabaseSchema(conn_url)
+    schema = DatabaseSchema(conn_url())
     schema.update()
     return redirect("/sysadmin/")
--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 # all config related stuff in this root module
 import os
 
-conn_url = os.environ.get('DATABASE_URL', 'postgresql://dev_role:dev_role@localhost:5432/flask_bitchez')
+
+def env_value(app, env_var, conf_key, default):
+    value = os.environ.get(env_var, default)
+    app.config[conf_key] = value
 
 def heroku_config(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = conn_url
+    env_value(app, 'DATABASE_URL', 'SQLALCHEMY_DATABASE_URI','postgresql://dev_role:dev_role@localhost:5432/flask_bitchez')
     app.config['UPLOAD_ROOT_URL'] = 'http://jims-s3-testing-bucket2.s3-website-us-east-1.amazonaws.com/'
     app.config['S3_API_KEY'] = 'AKIAJX3DKI72JEK4AL6Q'
     app.config['S3_API_SECRET'] = "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"
